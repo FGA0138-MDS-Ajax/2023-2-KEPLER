70e346a11d0f03a45a5573923647cecf8064bfd8
Gustavo Alves <gu.gu80@outlook.com>
2023-10-16 18:36:05 -0300

    Cadastro simples

--- backend/Projeto_MDS/settings.py
+++ backend/Projeto_MDS/settings.py
@@ -143,3 +143,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
