1d1ffb73e33de19a8463a68bf8132e75d5fcf3c9
Fenelas <gabriel.fenelon.biel@gmail.com>
2023-11-03 16:25:40 -0300

    1 versão tela de perfil

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -6,12 +6,13 @@ function  Perfil () {
         <div className="fundo-Perfil">
             <div className="main-perfil">
                 <div className="left-perfil">
-                    <span>Editar Perfil</span>
-                    <span>Gerenciador de Falta</span>
-                    <button>Voltar</button>
+                    <h2> UNB </h2>
+                    <span className="Editar-perfil"> Editar Perfil</span><br></br><br></br><br></br><br></br><br></br>
+                    <span className="Geren-falt">Gerenciador de Falta</span><br></br>
+                    <button className="button-Perfil">Voltar</button>
                 </div>
                <div className="right-perfil">
-                    <h3>Materias Semestre</h3>
+                    <h3>Materias Semestre </h3>
                 
                 </div> 
             </div>
