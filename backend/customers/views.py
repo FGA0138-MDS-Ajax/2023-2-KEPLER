70bac117c5374449024ac45b282d8e61acce3e78
Johnny Lopes <johnnylopis@hotmail.com>
2023-11-04 23:21:17 -0300

    Criação de uma nova pasta de projeto usando o django e o htmx para o frontend

--- backend/customers/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import mixins, generics, permissions
-
-from .models import Customer
-from .serializers import CustomerSerializer
-
-class CustomerListAPIView(generics.ListAPIView):
-    serializer_class = CustomerSerializer
-    permission_classes = [permissions.AllowAny]
-    queryset = Customer.objects.all()
