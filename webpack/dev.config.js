df413d83fb7bc525b8af48b3dba1e01c891464ab
Johnny Lopes <johnnylopis@hotmail.com>
2023-11-06 22:24:48 -0300

    teste para nova pasta projeto

---
+++ webpack/dev.config.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./common.config');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/': 'http://django:8000',
+    },
+    // We need hot=false (Disable HMR) to set liveReload=true
+    hot: false,
+    liveReload: true,
+  },
+  client: {
+    webSocketURL: 'auto://0.0.0.0:0/ws', // note the `:0` after `0.0.0.0`
+  }
+});
