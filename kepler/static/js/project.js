d5e9ce9d7e8a72589034da462d44efc490ad5ce9
Johnny Lopes <johnnylopis@hotmail.com>
2023-11-11 16:39:02 -0300

    Mover templates pra fragmentos

--- kepler/static/js/project.js
+++ kepler/static/js/project.js
@@ -1,10 +1,1 @@
-import '../sass/project.scss';
-import './Login.js';
-import './Cadastro.js';
-import './Home.js';
-import './Fluxograma.js';
-import './Perfil.js';
-import './Grade.js';
-import './Materias.js';
-
 /* Project specific Javascript goes here. */
