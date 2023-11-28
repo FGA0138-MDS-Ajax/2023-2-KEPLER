7811b2b8534641d8f98323b01afa696f9a38c168
Gustavo Alves <gu.gu80@outlook.com>
2023-11-28 01:26:18 -0300

    Navbar Grade

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,6 +16,8 @@ const Navbar = () => {
 
           <Button component={NavLink} to='/Fluxogramas' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Fluxogramas</Button>
 
+          {access_token && (  <Button component={NavLink} to='/Grade' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Grade</Button>)}
+
           {access_token && ( <Button component={NavLink} to='/Materias' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' };}} sx={{ color: 'white', textTransform: 'none' }}>Matérias </Button> )}
 
           {access_token ? <Button component={NavLink} to='/dashboard' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Perfil</Button> : <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Entrar/Registrar</Button>}
