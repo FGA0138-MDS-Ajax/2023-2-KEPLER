91d946b7d78bd1d866cb05697147adad06855839
Gustavo Alves <gu.gu80@outlook.com>
2023-11-28 01:27:33 -0300

    Navbar Perfil

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
 
           {access_token && ( <Button component={NavLink} to='/Materias' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' };}} sx={{ color: 'white', textTransform: 'none' }}>Matérias </Button> )}
 
-          {access_token ? <Button component={NavLink} to='/dashboard' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Perfil</Button> : <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Entrar/Registrar</Button>}
+          {access_token ? <Button component={NavLink} to='/Perfil' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Perfil</Button> : <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#0f467e' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Entrar/Registrar</Button>}
 
 
 
